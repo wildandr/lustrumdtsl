5e4ff74f898cff839bb7bab20ccfc2518b8c2304
Darriel Markerizal <darrielmarkerizal@gmail.com>
2024-02-24 21:27:57 +0700

    change config backend

--- my-backend/config/database.js
+++ my-backend/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("lustrumdtsl", "root", null, {
+const sequelize = new Sequelize("lustrumdtsl", "root2", "Makanmakan3x*", {
     host: "localhost",
     dialect: "mysql",
 });
