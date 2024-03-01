48dd2eddf34c6ffb3197a160904a9ffe1bbe55c4
Wildan Dzaky Ramadhani <wildandzakyramadhani@mail.ugm.ac.id>
2024-03-01 01:15:18 +0000

    update server

--- my-backend/server.js
+++ my-backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
-const cors = require("cors");
-const port = 5001;
+const fs = require("fs");
 const https = require("https");
+const cors = require("cors");
+
+const app = express();
+const port = 5001;
 
 const userRouter = require("./routes/users");
 const teamRouter = require("./routes/teams");
@@ -13,7 +15,6 @@ const fcecRouter = require("./routes/fcec");
 const craftRouter = require("./routes/craft");
 
 app.use(cors());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -28,8 +29,15 @@ app.use("/", sbcRouter);
 app.use("/", fcecRouter);
 app.use("/", craftRouter);
 
-app.listen(port, () => {
+// Konfigurasi opsi untuk sertifikat SSL
+const options = {
+    key: fs.readFileSync("/home/wildandzakyramadhani/www.lustrumkmtsl.com.key"),
+    cert: fs.readFileSync("/home/wildandzakyramadhani/sectigo_lustrumkmtsl.com_crt.txt")
+};
+
+// Buat server HTTPS
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-module.export = app;
