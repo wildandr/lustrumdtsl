aa4f378e2fe17718a20e43d5a8600a3766abaee2
Whyaziz <rizkyaziz214@gmail.com>
2024-03-22 22:39:24 +0700

    feat(claproyex) : add claproyex page

---
+++ my-frontend/src/app/claproyex/page.tsx
@@ -0,0 +1,22 @@
+import Hero from "@/component/module/claproyex/Hero";
+import Tema from "@/component/module/claproyex/Tema";
+import ComingSoon from "@/component/module/claproyex/ComingSoon";;
+import FooterClap from "@/component/layout/FooterClap";
+
+export default function Home() {
+    const backgroundImage = {
+        backgroundImage: `url(/assets/claproyex/bg_pattren.png)`,
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        backgroundRepeat: "repeat",
+    };
+
+    return (
+        <div className="bg-[#D0DEC0]" style={backgroundImage}>
+            <Hero />
+            <Tema />
+            <ComingSoon />
+            <FooterClap />
+        </div>
+    );
+}
