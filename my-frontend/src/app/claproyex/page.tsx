418b28bf2d23f393ca40bcfbc1f89986e968c6cb
Whyaziz <rizkyaziz214@gmail.com>
2024-05-18 23:38:27 +0700

    fix : title, warna, banyak gatau apa aja

--- my-frontend/src/app/claproyex/page.tsx
+++ my-frontend/src/app/claproyex/page.tsx
@@ -1,22 +1,29 @@
 import Hero from "@/component/module/claproyex/Hero";
 import Tema from "@/component/module/claproyex/Tema";
-import ComingSoon from "@/component/module/claproyex/ComingSoon";;
+import ComingSoon from "@/component/module/claproyex/ComingSoon";
 import FooterClap from "@/component/layout/FooterClap";
 
-export default function Home() {
-    const backgroundImage = {
-        backgroundImage: `url(/assets/claproyex/bg_pattren.png)`,
-        backgroundSize: "contain",
-        backgroundPosition: "center",
-        backgroundRepeat: "repeat",
-    };
+import type { Metadata } from "next";
 
-    return (
-        <div className="bg-[#D0DEC0]" style={backgroundImage}>
-            <Hero />
-            <Tema />
-            <ComingSoon />
-            <FooterClap />
-        </div>
-    );
+export const metadata: Metadata = {
+  title: "Claproyex",
+  description: "",
+};
+
+export default function Home() {
+  const backgroundImage = {
+    backgroundImage: `url(/assets/claproyex/bg_pattren.png)`,
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+    backgroundRepeat: "repeat",
+  };
+
+  return (
+    <div className="bg-[#D0DEC0]" style={backgroundImage}>
+      <Hero />
+      <Tema />
+      <ComingSoon />
+      <FooterClap />
+    </div>
+  );
 }
--- my-frontend/src/app/lustrum/page.tsx
+++ my-frontend/src/app/lustrum/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "../../component/module/lustrum/Hero";
-import Tema from "../../component/module/lustrum/Tema"
+import Tema from "../../component/module/lustrum/Tema";
 import Timeline from "../../component/module/lustrum/Timeline";
 import Kegiatan from "../../component/module/lustrum/Kegiatan";
 import Isi from "../../component/module/lustrum/Isi";
@@ -7,18 +7,23 @@ import Event from "../../component/module/lustrum/Event";
 import Footer from "../../component/layout/Footer";
 import Sponsorship from "../../component/module/lustrum/Sponsorship";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "LUSTRUM KMTSL",
+  description: "",
+};
 
 export default function Lustrum() {
-
   return (
     <div className="">
-        <Hero/>
-        <Tema/>
-        <Kegiatan/>
-        <Isi/>
-        <Event/>
-        <Sponsorship/>
-        <Footer/>
+      <Hero />
+      <Tema />
+      <Kegiatan />
+      <Isi />
+      <Event />
+      <Sponsorship />
+      <Footer />
     </div>
-  )
+  );
 }
